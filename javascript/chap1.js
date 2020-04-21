c236b48b928909901078e9dc0c779743458d1f6b
terry <vilver.terry@gmail.com>
2020-04-21 15:04:11 +0200

    use of indexOf();

--- javascript/chap1.js
+++ javascript/chap1.js
@@ -1,24 +1,16 @@
 //i'm a javascript comment
 /* as CSS comment, I'm also a JS */
 
-//Déclaration de variable
-var word;
-
-//initialisation
-word="string" ;
+//Déclaration + initialisation de variable
+var email = "terryvilver@gmail.com";
+var emailVerification = email.indexOf("@gmail.com");
+var click1 = document.getElementById("click1");
 
 function my_function(message) {
+    alert("position de l'élément recherché: " + emailVerification);
     console.log("event " + message);
 }
 
-document.getElementById("click1").onclick=function() {
+click1.onclick=function() {
     my_function("on click");
-}
-
-document.getElementById("click2").onclick=function() {
-    my_function("on click2");
-}
-
-document.getElementById("change").onchange=function() {
-    my_function("on change");
 }
