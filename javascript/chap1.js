797e26f089bb47d0746c209e73bf0b6de0e6f861
terry <vilver.terry@gmail.com>
2020-04-21 15:06:32 +0200

    add of some usefull comment

--- javascript/chap1.js
+++ javascript/chap1.js
@@ -6,11 +6,13 @@ var email = "terryvilver@gmail.com";
 var emailVerification = email.indexOf("@gmail.com");
 var click1 = document.getElementById("click1");
 
+//function
 function my_function(message) {
     alert("position de l'élément recherché: " + emailVerification);
     console.log("event " + message);
 }
 
+//appel de my_function lorsque j'appuie sur le bouton d'ID "click1".
 click1.onclick=function() {
     my_function("on click");
 }
