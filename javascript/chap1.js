c24c906325bb7278e62d1e7ae535e93387da13a8
vilver <vilver.terry@gmail.com>
2020-04-27 22:47:31 +0200

    while loop

--- javascript/chap1.js
+++ javascript/chap1.js
@@ -5,23 +5,22 @@
 var click1 = document.getElementById('click_buttom');
 
 //function
-function useIt() {
-    var email="terryvilvergmail.com"
-    var result=email.indexOf("@");
-    console.log("déterminons si : "+ email +" est bien une adresse mail.");
-    if (result != -1) {
-        console.log("result in position: " + result);
-        console.log(true+ " " + email + " est bien une adresse mail.");
+function loop() {
+    number = 5;
+    var target = document.getElementById("text");
+    target.innerHTML = "";
+    while (number > 0) {
 
-    } else {
-        console.log("result is not on the string");
-        console.log(false + " " + email + " n'est pas une adresse mail.");
-
-    }
+        target.innerHTML = target.innerHTML + "compte à rebours : " + number + "<br>";
+        console.log("compte à rebours : " + number);
+        number--;
+    } 
+    console.log("fin du compte à rebours !" );
 }
 
 
 //appel de email_test lorsque j'appuie sur le bouton d'ID "click1".
 click1.onclick=function() {
-    useIt();
+    loop();
+    this.style.color = "red";
 }
