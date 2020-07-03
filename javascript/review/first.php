315aa190d12e77356206b1677efa724901b1121f
vilver <vilver.terry@gmail.com>
2020-07-03 23:42:16 +0200

    php journey

---
+++ javascript/review/first.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <title></title>
+    </head>
+    
+    <body>
+        <h3>my first php page </h3>
+        
+        <?php print("My first sentence") ?>
+    </body>
+</html>
