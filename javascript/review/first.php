236d89b333c07f3e8fb17ccebd52c547ba809ff9
vilver <vilver.terry@gmail.com>
2020-07-04 23:54:05 +0200

    variable and use of MAMP

--- javascript/review/first.php
+++ javascript/review/first.php
@@ -9,6 +9,10 @@
         <h3>my first php page </h3>
 
 
-        <?php echo phpinfo(); ?>
+        <?php 
+            $a=10+10;
+            $a="mdp";
+            echo "a=" . $a;
+        ?>
     </body>
 </html>
