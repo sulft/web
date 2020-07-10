9cf054c0f393d2ddadb8967ff9d788d9b9820953
Terry Vilver <vilver.terry@gmail.com>
2020-07-10 22:22:15 +0200

    for loop an count function

--- php/first.php
+++ php/first.php
@@ -10,18 +10,26 @@
         
         <h3>my first php page </h3>
 
-
-        <?php 
-            $a= array("math","francais","anglais");
-            echo "nb element dans le tableau: " . count($a) . "<br>";
-            echo "test";
-        ?>
-
         <style>
-        <?php 
-            include "first.css";
-        ?>
+            <?php 
+                $b=0;
+                if($b) {
+                    include "first.css";
+                }
+                else {
+                    include "first1.css";
+                }
+            ?>
         </style>
+
+        <?php 
+            $a = array("banane","Pomme","Poire","jambon"); 
+            $aSize=count($a);
+
+            for($i = 0 ; $i < $aSize; $i++ ) {
+                echo "élément: " . ($i+1) . " de valeur: " . $a[$i] . "<br>";
+            }
+        ?>
 
     </body>
 </html>
