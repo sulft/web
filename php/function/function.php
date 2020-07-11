d333745e83981816492dc0ba01e2b425d5d0cc7e
Terry Vilver <vilver.terry@gmail.com>
2020-07-11 20:36:52 +0200

    foreach loop

--- php/function/function.php
+++ php/function/function.php
@@ -1,5 +1,7 @@
 <?php 
     $messageNumber = strlen("Terry");
+    $date = date("d/m/y");
+    $a = scandir("C:projet/web/php");
 ?>
 
 <!DOCTYPE html>
@@ -13,7 +15,17 @@
 
         <h1>PHP function</h1>
         <?php 
-            echo "le nombre de caractère est de " . $messageNumber . ".";
+            echo "le nombre de caractère est de " . $messageNumber . ".<br>";
+            echo "nous sommes le $date";
         ?>
+
+        <h1>Php loops</h1>
+        <?php
+            $array = array("test","marque",'odeur');
+            foreach($array as $value) {
+                echo $value . "<br>";
+            }
+        ?>
+
     </body>
 </html>
