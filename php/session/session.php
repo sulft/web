e4b2a36995e85d16b2732ed5ede73394eb8c8ea3
Terry Vilver <vilver.terry@gmail.com>
2020-07-12 17:45:12 +0200

    session php

---
+++ php/session/session.php
@@ -0,0 +1,15 @@
+<?php 
+    session_start()
+?>
+
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <title>Php session</title>
+    </head>
+
+    <body>
+        <h2>Les sessions php me permettent de tracker les gens.</h2>
+    </body>
+</html>
