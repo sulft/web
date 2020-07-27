d1470173347fb4bd5ebc63984e2b246ce3bc9aef
vilver <vilver.terry@gmail.com>
2020-07-28 00:06:29 +0200

    separing part from big box

--- javascript/exo1/manipulation.js
+++ javascript/exo1/manipulation.js
@@ -1,1 +1,17 @@
 //création tableau d'image
+var number;
+
+function loading_image() {
+    switch(number) {
+        case 1:
+
+            break;
+        case 2:
+
+            break;
+        default: 
+
+
+
+    }   
+]
