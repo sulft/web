126a4f650eaf4eac30f225e019055b09958fe93f
vilver <vilver.terry@gmail.com>
2020-12-30 12:36:22 +0100

    opération mathématique

---
+++ udemy/javascript/mathematique.js
@@ -0,0 +1,10 @@
+let nb1 = 3, nb2 = 2, result;
+result = nb1 + nb2;
+result = nb1 * nb2; 
+result = nb1 - nb2;
+result = nb1 / nb2;  
+result = nb1 % nb2;
+result = nb1 ** nb2;  
+result++; //incrémentation
+result--; //décrémentation
+console.log(result);
