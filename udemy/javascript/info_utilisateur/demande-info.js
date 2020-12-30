25bdcad262cf2df527b70b89d16a0ba55d783984
vilver <vilver.terry@gmail.com>
2020-12-30 14:20:30 +0100

    afficher, confirmer, demander info

---
+++ udemy/javascript/info_utilisateur/demande-info.js
@@ -0,0 +1,5 @@
+/* Demander une information à l'utilisateur à l'aide de prompt('');
+la fonction prompt() renvoie une valeur. 
+*/
+let age = prompt('Quel est votre âge ?');
+alert('Vous avez ' + age +' ans');
---
+++ udemy/javascript/info_utilisateur/information.js
@@ -0,0 +1,10 @@
+/* boite de dialogue*/
+
+if(confirm('Voulez-vous confirmer ?')/* Demande confirmation */) {
+    //ok
+    alert("OK !"); //affichage information
+} else {
+    //annulé
+    alert("Vous avez annulé !");
+}
+
