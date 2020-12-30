983d2d80fa139a5297bf5bcbb72ba95610c2e8b7
vilver <vilver.terry@gmail.com>
2020-12-30 12:16:13 +0100

    concatenation et utilisation deboguer vscode

---
+++ udemy/javascript/concatenation.js
@@ -0,0 +1,9 @@
+/* Concatenation */
+
+/* Challenge crée une variable prénom et l'afficher */
+let prenom = "Terry";
+let nom = "Vilver";
+
+/* Utiliser la concaténation */
+
+console.log('Bonjour ' + prenom + ' ' + nom + ' !');
