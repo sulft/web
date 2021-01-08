ebf3eaecd00ded6b2892aeda7318f231ab592fbf
vilver <vilver.terry@gmail.com>
2021-01-09 00:57:01 +0100

    switch en php

---
+++ udemy/php/condition/switch.php
@@ -0,0 +1,14 @@
+<?php 
+    //switch
+    $choix = true;
+
+    switch($choix) {
+        case true:
+            echo 'vrai';
+            break;
+
+        default:
+            echo "faux";
+            break;
+    }
+?>
