885b73c09711a91649fa249a79fb8bcdcdf9b416
vilver <vilver.terry@gmail.com>
2021-01-08 23:47:04 +0100

    structure if en php

---
+++ udemy/php/condition/if.php
@@ -0,0 +1,10 @@
+<?php 
+    //condition if
+    $age = 18;
+
+    if($age < 18) {
+        echo 'Tu as ' . $age .' ans. Tu es donc mineur !';
+    } else {
+        echo 'Tu as ' . $age . ' ans. Tu es donc majeur !';
+    }
+?>
