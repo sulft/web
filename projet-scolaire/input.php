e229f09fdaa4d0e7aa2687bdf792418e91f4ebee
vilver <vilver.terry@gmail.com>
2021-01-10 21:54:03 +0100

    projet informatique scolaire

--- projet-scolaire/input.php
+++ projet-scolaire/input.php
@@ -9,26 +9,19 @@
     <?php 
         try{
             // create a PostgreSQL database connection
-            $conn = new PDO('pgsql:host=localhost; dbname=postgres', 'postgres', 'Vjjvjj412*');
+            $conn = new PDO('pgsql:host=localhost; dbname=test', 'postgres', '');
             
             // display a message if connected to the PostgreSQL successfully
             if($conn){
                 echo "Connected successfully!";
+
+                $reponse = $conn->query("SELECT * from person");
             }
         }
         catch (PDOException $e){
             // report error message
             echo $e->getMessage();
         }
-        try {
-            $genre = $conn->query ("SELECT * from genre");
-            $pays = $conn->query ("SELECT * from pays");
-            $fruit = $conn->query ("SELECT * from fruit");
-        } catch(PDOException $e){
-            echo $e->getMessage();
-        }
-
-        echo $genre;
     ?>
 
     <form action="test.php" target="_blank" autocomplete="on" method="post">
@@ -52,26 +45,18 @@
             <input type="url" id="url" name="url">
         </label><br>
 
-        <label for="radio">Sexe :
-            <span>Homme</span><input type="radio" id="equipe" name="equipe">
-            <span>Femme</span><input type="radio" id="equipe" name="equipe">
+        <label for="sexe">Sexe :
+            <input type="radio" id="Homme" name="sexe" value="homme" required><label for="Homme">Homme</label>
+            <input type="radio" id="Femme" name="sexe" value='femme' required><label for="Femme">Femme</label>
         </label><br>
 
-        <label for="equipe"> fruit préféré:
-            <span>France</span><input type="checkbox" id="equipe" name="equipe">
-            <span>Russie</span><input type="checkbox" id="equipe" name="equipe">
-            <span>Russie</span><input type="checkbox" id="equipe" name="equipe">
-            <span>Japon</span><input type="checkbox" id="equipe" name="equipe">
-        </label><br>
-
-        <label for="pays">Pays de naissance</label>
-
-        <select name="pays" id="cars" size="datalist">
-            <option value="volvo">France</option>
-            <option value="saab">Russiee</option>
-            <option value="mercedes">Mercedes</option>
-            <option value="audi">Audi</option>
-        </select>
+        <p> Personne :
+            <select name="person" id="person" size="datalist">
+                <?php while($row1 = $reponse->fetch()):?>
+                <option name='' value = '<?php echo $row1[0]. " " . $row1[1]?>'><?php echo $row1[0];?> <?php echo $row1[1];?><?php ;?></option>
+                <?php endwhile;?>
+            </select>
+        </p>
 
         <label for="reinitialiser">
             <input type="reset" id="reinitialiser" name="reinitialiser">
