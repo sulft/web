e78c57db1ad82ead778828b68b22e8391d2dad2f
vilver <vilver.terry@gmail.com>
2021-01-11 22:14:19 +0100

    boucle while et do while

---
+++ udemy/javascript/boucle/while.js
@@ -0,0 +1,13 @@
+let i = 1;
+//la boucle while
+while ( i < 5) {
+    console.log('Lignes ' + i)
+    i++;
+}
+
+let j = 1;
+//la boucle do while
+do {
+    console.log('colonnes ' + j)
+    j++;
+} while (j <= 10);
