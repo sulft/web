f5ac3d88fddcd9d132103f4f1fc6b1fb75705b64
vilver <vilver.terry@gmail.com>
2021-01-11 21:56:50 +0100

    condition ternaire

--- udemy/javascript/condition/if.js
+++ udemy/javascript/condition/if.js
@@ -1,20 +1,28 @@
 //les condition if ... else
 
-/*
-let nombre = 12 ;
 
+let nombre = 12 ;
+/*
 if ( nombre < 11) {
     console.log("Dans le if !"); 
 } else { console.log("Dans le else ! ")}
 */
 
-//challenge vérifier l'age d'un utilisateur.
+// OU
+/* CONDITION TERNAIRE */
+nombre < 11 ? console.log("Dans le if !") : console.log("Dans le else ! ");
+
+
+
+
+//CHALLENGE vérifier l'age d'un utilisateur.
 /*
 -18 ans afficher => vous n'êtes pas majeur.
 entre 18 et 20 ans => vous êtes majeur en france.
 21+ => vous êtes majeur partout, à vous les casinos !
 */
-
+//REPONSE
+/*
 let age = 22
 
 if ( age<18 ) { 
@@ -24,3 +32,4 @@ if ( age<18 ) {
 } else {
     console.log("Vous êtes majeur partout, à vous les casinos !") 
 }
+*/
