d35a7417ab7f15017e127869dff2e56aca805be5
vilver <vilver.terry@gmail.com>
2021-01-12 13:42:29 +0100

    calculatrice en javascript projet

--- udemy/javascript/projet-calculatrice/code.js
+++ udemy/javascript/projet-calculatrice/code.js
@@ -1,43 +1,68 @@
 //projet calculatrice
 
+/* Mes variables*/
+let choix;
+let nombre1;
+let nombre2;
+
 /* choix du type de calcul */
-function choixCalcul() {
-    let choix;
-    do {
-        choix = prompt("Que souhaitez-vous faire ?\n\n"
-        + "1 - Addition \n"
-        + "2 - Multiplication\n"
-        + "3 - Soustraction\n"
-        + "4 - division\n"
-        );
-    }
-    while(choix<1 || choix>4);
-    return choix;
+do {
+     choix = prompt("Que souhaitez-vous faire ?\n\n"
+    + "1 - Addition \n"
+    + "2 - Multiplication\n"
+    + "3 - Soustraction\n"
+    + "4 - Division\n"
+    );
+}while((choix<1 || choix>4) || isNaN(choix));
+
+do {
+    nombre1= Number ( prompt("Entrez le premier nombre ? ") );
+    nombre2= Number ( prompt("Entrez est le second nombre ? ") );
+}while(isNaN(nombre1) || isNaN(nombre2))
+
+//Les fonctions de calculs
+function addition(nombreA, nombreB) {
+    return nombreA + nombreB;
+}
+
+function soustraction(nombreA, nombreB) {
+    return nombreA - nombreB;
 }
 
 
-//Selection du type de calcul
-function selectionCalcul() {
-    nombre = choixCalcul();
-    switch(nombre) {
+function multiplication(nombreA, nombreB) {
+    return nombreA * nombreB;
+}
+
+
+function division(nombreA, nombreB) {
+    if(nombreB == 0) {
+        throw new Error("Division par 0, impossible !");
+    }
+
+    return nombreA / nombreB;
+}
+
+try {
+    switch(Number(choix)) {
         case 1:
-            alert('test123');
+            resultat = addition(nombre1,nombre2);
             break;
         case 2:
-            alert('test');
+            resultat = multiplication(nombre1,nombre2);
             break;
         case 3:
-            alert('test');
+            resultat = soustraction(nombre1,nombre2);
             break;
         case 4:
-            alert('test');
+            resultat = division(nombre1,nombre2);
             break;
-    }
-}
+        default:
+            throw new Error("Une erreur est survenue !");
+    } 
+    alert("Voici le résultat : " + resultat);
+} catch(e) { alert(e) }
 
-selectionCalcul();
 
-//Demande des nombre à l'utilisateur
-/*let nombre1 = prompt("Entrer le premier nombre :");
-let nombre2 = prompt("Entrer le second nombre :");*/
+
 
