3a7938abb413341db702be9c3e6098a69edf459c
sulft <vilver.terry@gmail.com>
2021-01-15 16:26:56 +0100

    utilisation

--- angularJS/angularJS_modules/ctrl.js
+++ angularJS/angularJS_modules/ctrl.js
@@ -1,3 +1,9 @@
+//notion de $rootScope utilisable partout
+app.run(function($rootScope) {
+    $rootScope.test = 'test';
+  });
+
+//notion de $scope utilisable de façon locale
 app.controller('ctrl', function($scope) {
     $scope.nom = 'vilver';
     $scope.prenom = 'terry';
