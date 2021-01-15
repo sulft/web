9340e9b6dae3427eed91790ba15a360937003bde
sulft <vilver.terry@gmail.com>
2021-01-15 16:05:18 +0100

    tableau associatif et ng-repeat

--- angularJS/angularJS_modules/ctrl.js
+++ angularJS/angularJS_modules/ctrl.js
@@ -1,6 +1,11 @@
 app.controller('ctrl', function($scope) {
     $scope.nom = 'vilver';
     $scope.prenom = 'terry';
+    $scope.names = [
+        {name : 'John',     age : 12},
+        {name : 'Antoni',   age : 16}, 
+        {name : 'Hans',     age : 20}
+    ];
     $scope.complet = function() {
         return $scope.prenom + " " + $scope.nom;
     };
