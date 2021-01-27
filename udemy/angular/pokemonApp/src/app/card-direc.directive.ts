63d4a7cd2a895ce3233211dfa9ad2ba6b617b9b9
vilver <vilver.terry@gmail.com>
2021-01-28 00:21:33 +0100

    rendu code plus agréable

--- udemy/angular/pokemonApp/src/app/card-direc.directive.ts
+++ udemy/angular/pokemonApp/src/app/card-direc.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, ElementRef, HostListener } from '@angular/core';
+import { Directive, ElementRef, HostListener, Input } from '@angular/core';
 
 @Directive({
   selector: '[pkmnBorderCard]' 
@@ -9,23 +9,31 @@ export class CardDirecDirective {
 		this.setHeight(180);
 	}
 
+	initialColor:string = '#f5f5f5';
+	defaultColor:string = '#009688';
+	defaultHeight:number = 180;
+
+	@Input('pkmnBorderCard') borderColor: string;// déclaration de la propriété borderColor avec alias
+
 	//change la couleur de l'élément lorsque l'utilisateur passera son curseur sur le bloc
 	@HostListener('mouseenter') onMouseEnter() {
-		this.setBorder('#009688');
+		this.setBorder(this.borderColor ||this.defaultColor);
 	}
 
 	//change la couleur de l'élément lorsque l'utilisateur retire son curseur sur le bloc
 	@HostListener('mouseleave') onMouseLeave() {
-		this.setBorder('#f5f5f5');
+		this.setBorder(this.initialColor);
 	}
 
 
 	private setBorder(color: string) {
-		let border = 'solid 4px ' + color;
+		let border = 'dotted 4px ' + color;
 		this.el.nativeElement.style.border = border;
 	}
 
+
+
 	private setHeight(height: number) {
-		this.el.nativeElement.style.height = height + 'px';
+		this.el.nativeElement.style.height = this.defaultHeight + 'px';
 	}
 }
