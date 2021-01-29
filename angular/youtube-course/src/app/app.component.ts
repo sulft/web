08c75de5bed7de68ac8a0b5ba3efee52b8e33f5d
vilver <vilver.terry@gmail.com>
2021-01-30 00:39:44 +0100

    Recevoir des valeurs d'un input box

--- angular/youtube-course/src/app/app.component.ts
+++ angular/youtube-course/src/app/app.component.ts
@@ -6,9 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title:string = 'Bienvenu';
-  myEvent(evt) {
-    console.log(evt);
+  title:string = "Get input box value";
+  unTexte:string;
+  myEvent(text) {
+    this.unTexte = text;
   }
 
 }
