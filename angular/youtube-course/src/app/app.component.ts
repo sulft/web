0e094641491122c80a5fdbeeaf819958a3263f57
vilver <vilver.terry@gmail.com>
2021-01-31 23:38:52 +0100

    switch case condition en angular

--- angular/youtube-course/src/app/app.component.ts
+++ angular/youtube-course/src/app/app.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title:string = "Condition in angular";
-  show:string = 'blue';
+  title:string = "Switch case condition in angular";
+  color:string = 'blue';
 
 }
