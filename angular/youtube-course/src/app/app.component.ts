19b023b833cba618f8b713d63785de5e73081e17
vilver <vilver.terry@gmail.com>
2021-02-01 00:21:08 +0100

    simple forms in angular

--- angular/youtube-course/src/app/app.component.ts
+++ angular/youtube-course/src/app/app.component.ts
@@ -6,28 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title:string = "for loop in Angular";
-  data:string[]=["Terry",'Sam','Peter','John'];
-  array:Object[]=[
-    {
-      nom:'Terry',
-      age:'18',
-      email:'xyz@test.com'
-    },
-    {
-      nom:'Sam',
-      age:'22',
-      email:'xyz@test.com'
-    },
-    {
-      nom:'Peter',
-      age:'17',
-      email:'xyz@test.com'
-    },
-    {
-      nom:'John',
-      age:'28',
-      email:'xyz@test.com'
-    }
-  ]
+  title:string = "Simple form in Angular";
+
+
+  getValues(val:any) {
+    console.log(val)
+  }
 }
--- angular/youtube-course/src/app/app.module.ts
+++ angular/youtube-course/src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+
 
 //module
 
@@ -11,6 +13,7 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    FormsModule
 
   ],
   providers: [],
