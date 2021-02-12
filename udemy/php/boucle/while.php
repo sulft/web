544a25c897cee294d2dd7cfa661c521396c53007
vilver <vilver.terry@gmail.com>
2021-02-12 23:59:15 +0100

    programme while boucle

---
+++ udemy/php/boucle/while.php
@@ -0,0 +1,9 @@
+<?php 
+    //challenge afficher voici le numéro de la ligne en commençant par 1
+    $ligne = 1;
+
+    while($ligne < 10) {
+        echo 'Voici le numéro de la ligne ' . $ligne . '<br>';
+        $ligne++;
+    }
+?>
