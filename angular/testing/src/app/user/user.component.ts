d6c0611588447de348ccd8629e0ced6860819172
vilver <vilver.terry@gmail.com>
2021-05-11 22:11:51 +0200

    testing unit

---
+++ angular/testing/src/app/user/user.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-user',
+  templateUrl: './user.component.html',
+  styleUrls: ['./user.component.css']
+})
+export class UserComponent implements OnInit {
+
+  constructor() { }
+  componentName="user";
+  ngOnInit(): void {
+  }
+
+  sum() {
+    //
+    //
+    //
+    return 100;
+  }
+
+}
