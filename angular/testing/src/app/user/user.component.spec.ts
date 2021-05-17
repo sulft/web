fe6290cea1e89b63897c447589d54bca3b2c9ab8
vilver <vilver.terry@gmail.com>
2021-05-17 12:01:08 +0200

    adding gitignore et modification testing

--- angular/testing/src/app/user/user.component.spec.ts
+++ angular/testing/src/app/user/user.component.spec.ts
@@ -1,39 +1,50 @@
-import { toBase64String } from '@angular/compiler/src/output/source_map';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { UserComponent } from './user.component';
 
-describe('UserComponent', () => {
+describe('UserComponent', function() {
   let component: UserComponent;
   let fixture: ComponentFixture<UserComponent>;
 
-  beforeEach(async () => {
+  beforeEach(async function() {
     await TestBed.configureTestingModule({
       declarations: [ UserComponent ]
     })
     .compileComponents();
   });
 
-  beforeEach(() => {
+  beforeEach(function() {
     fixture = TestBed.createComponent(UserComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
 
-  it('calculation', ()=> {
-    expect(component.sum(40,60)).toBe(100);
-  });
+  describe("mes tests", function() {
+    it("let's check amount", function() {
+      expect(100).toBe(100)
+    });
+    
+    it("let's check string", function() {
+      expect("test").toBe("test")
+    });
 
-  it("test html element", () => {
-    const data=fixture.nativeElement;
-    expect(data.querySelector(".some span").textContent).toContain("User");
-  });
+    it('should create', function() {
+      expect(component).toBeTruthy();
+    });
+  
+    it('calculation', function() {
+      expect(component.sum(40,60)).toBe(100);
+    });
+  
+    it("test html element", function() {
+      const data=fixture.nativeElement;
+      expect(data.querySelector(".some span").textContent).toContain("User");
+    });
 
-  describe("dummy test", ()=> {
-    it("let's check amount", ()=> expect(100).toBe(100));
+    it("test html element 222", function() {
+      const data=fixture.nativeElement;
+      expect(data.querySelector("span").textContent);
+    });
   });
 });
