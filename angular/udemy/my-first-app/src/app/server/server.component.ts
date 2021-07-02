d29baa54e12d6170b2c3dd79879daac8bb648e60
vilver <vilver.terry@gmail.com>
2021-07-02 02:15:58 +0200

    ajout d'un nouveau composant dans mon AppModule

--- angular/udemy/my-first-app/src/app/server/server.component.ts
+++ angular/udemy/my-first-app/src/app/server/server.component.ts
@@ -1,9 +1,11 @@
-import { Component } from '@angular/core'
+import { Component } from "@angular/core";
+
 
 @Component({
-    selector:'app-server',
-    templateUrl:'./server.component.html'
-}) 
+    selector:"app-server",
+    templateUrl:"./server.component.html"
+
+})
 
 export class ServerComponent {
 
