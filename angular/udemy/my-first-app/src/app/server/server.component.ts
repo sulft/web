d39afe3c4b9082522ecf736abfca9280e3bd98fe
vilver <vilver.terry@gmail.com>
2021-07-03 17:42:22 +0200

    using ngClass to add class with conditions..

--- angular/udemy/my-first-app/src/app/server/server.component.ts
+++ angular/udemy/my-first-app/src/app/server/server.component.ts
@@ -3,8 +3,8 @@ import { Component } from "@angular/core";
 
 @Component({
     selector:"app-server",
-    templateUrl:"./server.component.html"
-
+    templateUrl:"./server.component.html",
+    styles:['.online { color:white }']
 })
 
 export class ServerComponent {
