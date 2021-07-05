0075ce6701499ef5f97033601da9eb5326745fd8
vilver <vilver.terry@gmail.com>
2021-07-05 02:14:19 +0200

    creating navbar and first model

--- angular/udemy/projectCuisine/src/app/app.module.ts
+++ angular/udemy/projectCuisine/src/app/app.module.ts
@@ -8,6 +8,7 @@ import { HeaderComponent } from './header/header.component';
 import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { ShoppingListEditComponent } from './shopping-list/shopping-list-edit/shopping-list-edit.component';
+import { RecipesComponent } from './recipes/recipes.component';
 
 @NgModule({
   declarations: [
@@ -17,8 +18,9 @@ import { ShoppingListEditComponent } from './shopping-list/shopping-list-edit/sh
     HeaderComponent,
     RecipeItemComponent,
     ShoppingListComponent,
-    ShoppingListEditComponent
-    
+    ShoppingListEditComponent,
+    RecipesComponent
+
   ],
   imports: [
     BrowserModule
