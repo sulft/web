913ec0d49dd8cb0adacf209605b0fdeb9349ab31
vilver <vilver.terry@gmail.com>
2021-07-09 03:22:39 +0200

    custome event binding use

--- angular/udemy/databinding/src/app/app.component.ts
+++ angular/udemy/databinding/src/app/app.component.ts
@@ -18,7 +18,21 @@ export class AppComponent {
     content:'Je suis un test'
   }
 ];
-  newServerName = '';
-  newServerContent = '';
+
+  onServerAdded(serverData:{serverName:string, serverContent:string}) {
+    this.serverElements.push({
+      type: 'server',
+      name: serverData.serverName,
+      content: serverData.serverContent
+    });
+  }
+
+  onBlueprintAdded(blueprintData:{serverName:string, serverContent:string}) {
+    this.serverElements.push({
+      type: 'blueprint',
+      name: blueprintData.serverName,
+      content: blueprintData.serverContent
+    });
+  }
 
 }
