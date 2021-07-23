cb1441f6c4ba75bcb29369b7bce43b9ec47dbec4
vilver <vilver.terry@gmail.com>
2021-07-23 02:24:18 +0200

    passing data to child component

--- angular/udemy/projectCuisine/src/app/recipes/recipe-list/recipe-list.component.ts
+++ angular/udemy/projectCuisine/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -9,7 +9,7 @@ import { Recipe } from '../recipe.model';
 export class RecipeListComponent implements OnInit {
   
   recipes:Recipe[] = [
-    new Recipe('A test recipe', 'This a test', 'https://realfood.tesco.com/media/images/RFO-October2020-65809-Tesco-LetsCook-Oct20-65850-SpicedChickenGreenBeans1400x919-38f3e9b0-7241-49a3-83fe-fcc38d2c24be-0-1400x919.jpg'),
+    new Recipe('A test recipe', 'This a test', 'https://cdn.loveandlemons.com/wp-content/uploads/2020/03/pantry-recipes-2-580x697.jpg'),
     new Recipe('Pasta','Recipe of Pasta','https://cdn.loveandlemons.com/wp-content/uploads/2020/03/pantry-recipes-2-580x697.jpg')
   ];
 
