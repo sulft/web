da21ad861f7a22d7c4cd7bcca347dfb27d84352f
vilver <vilver.terry@gmail.com>
2021-07-29 23:51:10 +0200

    utilisation de hostListener pour appliquer un style en cas d'evenement

--- angular/playingAround/vitrine/src/app/myDirective/new-directive.directive.ts
+++ angular/playingAround/vitrine/src/app/myDirective/new-directive.directive.ts
@@ -1,13 +1,21 @@
-import { Directive, ElementRef, OnInit, Renderer2 } from '@angular/core';
+import { Directive, ElementRef, HostListener, OnInit, Renderer2 } from '@angular/core';
 
 @Directive({
   selector: '[appNewDirective]'
 })
 export class NewDirectiveDirective implements OnInit {
-
   constructor(private elRef: ElementRef, private renderer:Renderer2) { }
 
   ngOnInit() {
+    // this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'blue');
+  }
+
+  @HostListener('mouseenter') mouseover(eventData: Event) {
     this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'blue');
+    console.log(eventData)
+  }
+
+  @HostListener('mouseleave') mouseleave(eventData: Event) {
+    this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'transparent');
   }
 }
