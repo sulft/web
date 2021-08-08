97c0e5b7187c74c926d2de9d50803b0a89bb0375
vilver <vilver.terry@gmail.com>
2021-08-08 23:14:21 +0200

    passage de donnée via les services

--- angular/udemy/services-start/src/app/new-account/new-account.component.ts
+++ angular/udemy/services-start/src/app/new-account/new-account.component.ts
@@ -6,17 +6,21 @@ import { LoggingService } from '../logging.service';
   selector: 'app-new-account',
   templateUrl: './new-account.component.html',
   styleUrls: ['./new-account.component.css'],
-  providers: [LoggingService, AccountsService]
+  // providers: [LoggingService]
 })
 export class NewAccountComponent {
 
   constructor( 
     private loggingService: LoggingService,
     private accountsService: AccountsService
-  ) {}
+  ) {
+    this.accountsService.statusUpdated.subscribe(
+      (status:string) => alert('New status: ' + status)
+    )
+  }
 
   onCreateAccount(accountName: string, accountStatus: string) {
     this.accountsService.addAccount(accountName,accountStatus);
-    this.loggingService.logStatusChange(accountStatus);
+    // this.loggingService.logStatusChange(accountStatus);
   }
 }
