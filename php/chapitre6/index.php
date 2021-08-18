b47fcd0c73b9ff65eaf5701f6a42cec21aa29669
vilver <vilver.terry@gmail.com>
2021-08-18 17:07:40 +0200

    utilisation de fonction

---
+++ php/chapitre6/index.php
@@ -0,0 +1,10 @@
+<?php 
+    //Les FONCTIONS
+    function sayHello($prenom,$nom) {
+        echo 'Bonjour ' . $nom . ' '. $prenom .'<br>';
+    }
+
+    sayHello("Terry",'Vilver');
+    sayHello("Nicolas", '');
+
+?>
