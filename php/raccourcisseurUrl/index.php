327c832b98630108390713b339a20072f70377df
vilver <vilver.terry@gmail.com>
2021-09-15 00:48:31 +0200

    fin mini projet Udemy

--- php/raccourcisseurUrl/index.php
+++ php/raccourcisseurUrl/index.php
@@ -1,5 +1,58 @@
 <?php 
+    // A recu le raccourci
+    if(isset($_GET['q'])) {
+        $raccourci = htmlspecialchars($_GET['q']);
 
+        $bdd = new PDO('mysql:host=localhost;dbname=bitly;charset=utf8', 'root','');
+        $req = $bdd -> prepare('SELECT COUNT(*) AS x FROM link WHERE shortcut = ?');
+        $req -> execute([$raccourci]);
+
+        while ($result = $req -> fetch()) {
+            if($result['x'] != 1) {
+                header('location: ../?error=true&message=Adresse url non connue');
+                exit();
+            }
+        }
+
+        //redirection
+        $req = $bdd -> prepare('SELECT * FROM link WHERE shortcut = ? ');
+        $req -> execute([$raccourci]);
+        while ($result = $req -> fetch()) {
+            header('location:'. $result['url']);
+            exit();
+        }
+    }
+
+    if(!empty($_POST['url'])) {
+        $url = $_POST['url'];
+
+        //verification
+        if (filter_var($url, FILTER_VALIDATE_URL)) {
+            echo("URL est valide");
+        } else {
+            header('location: ../?error=true&message=Adresse url non valide');
+            exit();
+        }
+        //raccourci
+        $raccourci = crypt($url, rand());
+
+        //deja envoyé
+        $bdd = new PDO("mysql:host=localhost;dbname=bitly;charset=utf8", 'root', '');
+        $req = $bdd -> prepare('SELECT COUNT(*) AS x FROM link WHERE url = ?');
+        $req -> execute([$url]);
+        while ($result = $req -> fetch()) {
+            if($result['x'] != 0) {
+                header('location: ../?error=true&message=Adresse déjà raccourcie');
+                exit();
+            }
+        }
+
+        //envoyé
+        $req = $bdd -> prepare('INSERT INTO link(url, shortcut) VALUES (?,?)');
+        $req ->execute([$url,$raccourci]);
+        header('location: ../?short='.$raccourci);
+        exit();
+    }
 ?>  
 
 <!DOCTYPE html>
@@ -27,6 +80,23 @@
                         <input type="url" name="url" placeholder='Entrez votre lien'>
                         <button type="submit">Raccourcir</button>
                     </form>
+
+                    <?php 
+                        if(isset($_GET['error']) && isset($_GET['message'])) { ?>
+                        <div class="center">
+                            <div id="result">
+                                <b><?php echo htmlspecialchars($_GET['message']) ?></b>
+                            </div>
+                        </div>
+                    <?php } 
+                        else if(isset($_GET['short'])) {  ?>
+                            <div class="center">
+                            <div id="result">
+                                <b>Url raccourcie : </b> http://localhost/?q=<?php echo htmlspecialchars($_GET['short']); ?>
+                            </div>
+                        </div>
+                    <?php } ?>
+                    
                 </div>
             </div>
         </section>
@@ -41,15 +111,11 @@
             </div>
         </section>
 
-        <section id='footer'>
-            <div class="container">
-                <div class="center">
-                    <img src="./pictures/logo2.png" alt="logo" id="logo">
-                </div>
-                <p>2018 bitly</p>
-                <p><span class="orange"> Contact </span> . <span class="orange"> A Propos </span></p>
-            </div>
-        </section>
+        <footer>
+            <img src="./pictures/logo2.png" alt="logo" id="logo">
+            <p>2018 © bitly</p>
+            <a href="#">Contact</a> - <a href="#"> A propos</a>
+        </footer>
     </body>
 
 </html>
