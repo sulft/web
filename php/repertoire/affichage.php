e0223d963549b2be37b84ea3a14e9f4aa4d95b15
vilver <vilver.terry@gmail.com>
2021-09-16 18:44:52 +0200

    refactoring

--- php/repertoire/affichage.php
+++ php/repertoire/affichage.php
@@ -4,53 +4,38 @@
 
     if(!empty($_POST['email']) && empty($_POST['tel'])) {
         $email = $_POST['email'];
-        if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {  
-            header('location: ./?error=true&message=Vous n\'avez pas rempli correctement ce champ');
-            exit();
+        checkEmail($email);
+        if(!empty($_POST['prenom']) && !empty($_POST['nom'])) {
+            $prenom = checkPrenom();
+            $nom = checkNom();
+            $que = $bdd -> prepare("INSERT INTO contact (prenom,nom,email) VALUES (?,?,?)");
+            $que -> execute([$prenom,$nom,$email]);
         } else {
-            if(!empty($_POST['prenom']) && !empty($_POST['nom'])) {
-                checkPrenom();
-                checkNom();
-                $que = $bdd -> prepare("INSERT INTO contact (prenom,nom,tel) VALUES (?,?,?)");
-                $que -> execute([$prenom,$nom,$tel]);
-            } else {
-                header('location: ./?error=true&message=Vous n\'avez pas rempli tout les champs obligatoire');
-                exit();
-            }
+            header('location: ./?error=true&message=Vous n\'avez pas rempli tout les champs obligatoire');
+            exit();
         }
     }
 
     if(!empty($_POST['tel']) && empty($_POST['email'])) {
         $tel = $_POST['tel'];
-        if(!preg_match ("/^(?:(?:\+|00)33|0)\s*[1-9](?:[\s.-]*\d{2}){4}$/", $tel) ) {  
-            header('location: ./?error=true&message=Vous n\'avez pas rempli correctement ce champ');
-            exit();       
-        } else{
-            if(!empty($_POST['prenom']) && !empty($_POST['nom'])) {
-                checkPrenom();
-                checkNom();
-                $que = $bdd -> prepare("INSERT INTO contact (prenom,nom,tel) VALUES (?,?,?)");
-                $que -> execute([$prenom,$nom,$tel]);
-            } else {
-                header('location: ./?error=true&message=Vous n\'avez pas rempli tout les champs obligatoire');
-                exit();
-            }
+        checkTel($tel);
+        if(!empty($_POST['prenom']) && !empty($_POST['nom'])) {
+            $prenom = checkPrenom();
+            $nom = checkNom();
+            $que = $bdd -> prepare("INSERT INTO contact (prenom,nom,tel) VALUES (?,?,?)");
+            $que -> execute([$prenom,$nom,$tel]);
+        } else {
+            header('location: ./?error=true&message=Vous n\'avez pas rempli tout les champs obligatoire');
+            exit();
         }
+        
     }
 
     if(!empty($_POST['tel']) && !empty($_POST['email'])) {
         $tel = $_POST['tel'];
         $email = $_POST['email'];
-        if(!preg_match ("/^(?:(?:\+|00)33|0)\s*[1-9](?:[\s.-]*\d{2}){4}$/", $tel) ) {  
-            header('location: ./?error=true&message=Vous n\'avez pas rempli correctement ce champ');
-            exit();       
-        }
-
-        if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {  
-            header('location: ./?error=true&message=Vous n\'avez pas rempli correctement ce champ');
-            exit();
-        }
-
+        checkTel($tel);
+        checkEmail($email);
         if(!empty($_POST['prenom']) && !empty($_POST['nom'])) {
             checkPrenom();
             checkNom();
--- php/repertoire/check.php
+++ php/repertoire/check.php
@@ -4,8 +4,11 @@
             $prenom = $_POST['prenom'];
             if(!preg_match("/^[0-9a-zA-Z ]*$/",$prenom)) {
                 header('location: ./?error=true&message=Vous n\'avez pas rempli correctement ce champ');
-                exit();
+                exit(); 
+            } else {
+                return $prenom;
             }
+
         }
     }
 
@@ -15,7 +18,23 @@
             if(!preg_match("/^[0-9a-zA-Z ]*$/",$nom)) {
                 header('location: ./?error=true&message=Vous n\'avez pas rempli correctement ce champ');
                 exit();
+            } else {
+                return $nom;
             }
+        }
+    } 
+
+    function checkTel($tel) {
+        if(!preg_match ("/^(?:(?:\+|00)33|0)\s*[1-9](?:[\s.-]*\d{2}){4}$/", $tel) ) {  
+            header('location: ./?error=true&message=Vous n\'avez pas rempli correctement ce champ');
+            exit();       
+        }
+    }
+
+    function checkEmail($email) {
+        if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {  
+            header('location: ./?error=true&message=Vous n\'avez pas rempli correctement ce champ');
+            exit();
         }
     }
 ?>
