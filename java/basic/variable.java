e70e64f6cfda108685bd6667e7b8c768464c10d4
vilver <vilver.terry@gmail.com>
2021-09-17 00:25:37 +0200

    maitrise des variables

---
+++ java/basic/variable.java
@@ -0,0 +1,15 @@
+class Variable {
+    public static void main(String[] args) {
+        int a = 1;
+        double b = 1.1d;
+        float c = 1.1f;
+        boolean d = true;
+        String phrase = "une phrase";
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(phrase);
+    }
+}
