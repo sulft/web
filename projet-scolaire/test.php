787b5a0999418f3201424cfdca5f2f5db23545df
vilver <vilver.terry@gmail.com>
2022-02-11 01:13:24 +0100

    cleaning directory

--- projet-scolaire/test.php
+++
@@ -1,9 +0,0 @@
- <?php 
-        echo "Bonjour ! " . $_POST['person'] . " !<br><br>";
-        echo "Votre identifiant est : ". $_POST['identifiant'] . "<br><br>";
-        echo "Vous êtes un(e) " . $_POST['sexe'] . " ! <br><br>";
-        echo "Votre mot de passe est : " . $_POST['mdp'] . " <br>OOPS! J'ai dévoilé votre mot de passe !"."<br><br>";
-        echo "La date entrée est " . $_POST['date'] . "<br><br>";
-        echo "Votre email est : " . $_POST['email'] . "<br><br>";
-        echo "Votre url est : " . $_POST['url'] . "<br><br>";
-?>
