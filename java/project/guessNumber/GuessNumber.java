e18248a4f3f1a23a962fb6753d5ddd5a235a91db
vilver <vilver.terry@gmail.com>
2022-03-02 00:22:03 +0100

    jeu amélioré

--- java/project/guessNumber/GuessNumber.java
+++ java/project/guessNumber/GuessNumber.java
@@ -1,3 +1,47 @@
+// import java.util.*;
+
+// public class GuessNumber{
+//     public static void main(String args[]) {
+//         Random rand = new Random();
+//         Scanner sc = new Scanner(System.in);
+//         int valeurMax = 50;
+//         String valeurUtilisateur = "";
+//         String nombreADeviner = String.valueOf(rand.nextInt(valeurMax+1));
+//         int nbEssai = 5;
+
+//         System.out.println("le nombre a deviner: " + nombreADeviner);
+
+//         while(nbEssai != 0) {
+//             System.out.print("Entrez un nombre compris entre 0 et " + valeurMax + " : ");
+//             valeurUtilisateur = sc.next();
+
+//             System.out.print("\n");
+
+//             if(valeurUtilisateur.equals(nombreADeviner)) {
+//                 System.out.print("Bravo");
+//                 break;
+//             }
+//             if(valeurUtilisateur.equals("r") || valeurUtilisateur.equals("R")) {
+//                 System.out.print("Vous avez abandonné !");
+//                 break;
+//             }
+            
+//             nbEssai--;
+
+//             if(nbEssai != 0 && !(valeurUtilisateur.equals("r") || valeurUtilisateur.equals("R")) ) {
+//                 System.out.println("Pour arrêter de jouer, entrez 'r' ou 'R' !");
+//             }
+//         }
+
+
+//         if(nbEssai == 0) {
+//             System.out.println("Vous avez perdu..");
+//         }
+
+//         sc.close();
+//     }
+// }
+
 import java.util.*;
 
 public class GuessNumber{
@@ -5,29 +49,48 @@ public class GuessNumber{
         Random rand = new Random();
         Scanner sc = new Scanner(System.in);
         int valeurMax = 50;
-        String valeurUtilisateur;
+        String valeurUtilisateur = "";
         String nombreADeviner = String.valueOf(rand.nextInt(valeurMax+1));
         int nbEssai = 5;
 
         System.out.println("le nombre a deviner: " + nombreADeviner);
 
         while(nbEssai != 0) {
-            System.out.println("Pour arrêter de jouer, entrez 'r' ou 'R' !");
             System.out.print("Entrez un nombre compris entre 0 et " + valeurMax + " : ");
             valeurUtilisateur = sc.next();
-
-            System.out.print("\n");
-
 
             if(valeurUtilisateur.equals(nombreADeviner)) {
                 System.out.print("Bravo");
                 break;
-            }
+            } 
             if(valeurUtilisateur.equals("r") || valeurUtilisateur.equals("R")) {
                 System.out.print("Vous avez abandonné !");
                 break;
             }
+            if(!valeurUtilisateur.equals("r") || !valeurUtilisateur.equals("R")) {
+                System.out.print("Erreur de saisie ! Le jeu a été interrompu !");
+                break;
+            }
+            if(Integer.valueOf(nombreADeviner) > Integer.valueOf(valeurUtilisateur)) {
+                System.out.println("Le nombre est plus grand.");
+            }
+            if(Integer.valueOf(nombreADeviner) < Integer.valueOf(valeurUtilisateur)) {
+                System.out.println("Le nombre est plus petit.");
+            }
+
+            System.out.print("\n");
+
+
+            // if(valeurUtilisateur.equals(nombreADeviner)) {
+            //     System.out.print("Bravo");
+            //     break;
+            // }
+            
             nbEssai--;
+
+            if(nbEssai != 0 && !(valeurUtilisateur.equals("r") || valeurUtilisateur.equals("R")) ) {
+                System.out.println("Pour arrêter de jouer, entrez 'r' ou 'R' !");
+            }
         }
 
 
