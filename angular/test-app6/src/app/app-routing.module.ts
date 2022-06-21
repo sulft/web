941a3814c754396b6f61a2eddef0e8ce7ea3cd48
vilver <vilver.terry@gmail.com>
2022-06-22 00:09:26 +0200

    advanced rooting

--- angular/test-app6/src/app/app-routing.module.ts
+++ angular/test-app6/src/app/app-routing.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { Com1Component } from './com1/com1.component';
-import { StudentComponent } from './student/student.component';
 
 const routes: Routes = [
   { path: 'routeA', component: Com1Component },
