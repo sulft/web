8a4ccefc81324c80ae6a290b89e3a9f6186a5ad8
vilver <vilver.terry@gmail.com>
2022-07-03 15:51:26 +0200

    encapsulation et input

--- angular/angular-up-running/stock-market/src/app/app.component.ts
+++ angular/angular-up-running/stock-market/src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { Stock } from './model/stock';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  encapsulation: ViewEncapsulation.None
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'stock-market';
+
+  stockObj!:Stock;
+
+  ngOnInit() {
+    this.stockObj =  new Stock(
+      "Test Stock Company",
+      "TSC",
+      85,
+      80
+    )
+  }
+  
 }
--- angular/angular-up-running/stock-market/src/app/stock/stock-item/stock-item.component.ts
+++ angular/angular-up-running/stock-market/src/app/stock/stock-item/stock-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { Stock } from 'src/app/model/stock';
 
 @Component({
@@ -9,37 +9,17 @@ import { Stock } from 'src/app/model/stock';
 export class StockItemComponent implements OnInit {
 
 
-  // stock!:Stock
-  stocks!:Stock[]
-   /* public stockClasses:any;*/
-   public stockStyles:any;
+  @Input() stock!:Stock
+
 
   constructor() { }
 
   ngOnInit(): void {
-    this.stocks = [
-      new Stock('Test Stock company','TSC', 90, 80),
-      new Stock('Second Stock company','SSC', 10, 20),
-      new Stock('Third Stock company','LSC', 876, 765)
-    ]
-    // new Stock('Test Stock company','TSC', 90, 80);
-    // let diff = (this.stock.price / this.stock.previousPrice) - 1;
-    // console.log(diff);
-    // let largeChange = Math.abs(diff) > 0.05;
-    /*this.stockClasses = {
-      "negative":!this.stock.isPositiveChange(),
-      "positive":this.stock.isPositiveChange(),
-      "large-change": largeChange,
-      "small-change": !largeChange
-    }*/
-    // this.stockStyles = {
-    //   "font-size": largeChange ? '1.2em' : '0.8em',
-    //   "color": this.stock.isPositiveChange() ? 'green' : 'red'
-    // }
+
   }
 
-  toggleFavorite(event:any, index:number) {
-    console.log('We are toggling the favorite state for this stock', index,event);
-    this.stocks[index].favorite = !this.stocks[index].favorite;
+  toggleFavorite(event:any) {
+    console.log('We are toggling the favorite state for this stock',event);
+    this.stock.favorite = !this.stock.favorite;
   }
 }
