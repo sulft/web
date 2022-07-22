becbbec55f630d8c45f4f712c19a67f09fd18132
vilver <vilver.terry@gmail.com>
2022-07-22 23:11:00 +0200

    simulation fetch up donné from server pour envoyé au form de 2 manière, assign form value à l'instance,

--- angular/angular-up-running/reactiveForm/src/app/app.component.ts
+++ angular/angular-up-running/reactiveForm/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { FormControl, FormGroup, Validators, FormBuilder } from '@angular/forms';
 import { Person } from 'src/assets/models/person';
 
 @Component({
@@ -10,21 +10,47 @@ import { Person } from 'src/assets/models/person';
 export class AppComponent implements OnInit {
   title = 'reactiveForm';
 
+  get name() { return this.personForm.get('name') }
+  get age() { return this.personForm.get('age') }
+
   person:Person
   
   public personForm: FormGroup;
 
-  ngOnInit() {
-    this.person = new Person("Terry",24);
+  constructor(private fb:FormBuilder) {
 
-    this.personForm = new FormGroup({
-      namePerson: new FormControl(this.person.name, Validators.required),
-      agePerson: new FormControl(0, [Validators.required, Validators.min(0), Validators.max(120)])
+  }
+
+  ngOnInit() {
+    this.personForm = this.fb.group({
+      name: ["", Validators.required],
+      age: [0, [Validators.required, Validators.min(0), Validators.max(120)]]
     });
-    
+    this.person = new Person("test",24);
+  }
+
+  resetForm() {
+    this.personForm.reset();
+  }
+
+  loadStockFromServer() {
+    this.person = new Person("Terry",24);
+    let PersonFormModel = Object.assign({},this.person);
+    console.log(PersonFormModel);
+    /* delete PersonFormModel.address; Si des attributs sont des constantes inutiles de le copier
+     dans un objet une instance qui le possède dejà */
+    this.personForm.setValue(PersonFormModel);
+    /* ICI on attribue au form les valeurs ci dessus */
+  }
+
+  patchPersonForm() {
+    this.person = new Person("Terry",24);
+    this.personForm.patchValue(this.person);
+    console.log(this.person)
   }
 
   onSubmit() {
-
+    this.person = Object.assign({},this.personForm.value); /* on envoie plutot une copie des valeurs valeurs du Form, c'est un best practice */
+    console.log(this.person);
   }
 }
