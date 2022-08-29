8b62a0bfe074e7843346066a7e11db29ca036ca9
vilver <vilver.terry@gmail.com>
2022-08-30 00:05:04 +0200

    a commit

---
+++ udemy/javascript/jsFundamental/app.js
@@ -0,0 +1,15 @@
+// create an array
+const numbers = [10,25,30,50];
+const numbers2 = new Array();
+const fruit = ["banana","apple",'orange'];
+
+console.log(fruit);
+console.log(fruit[1]);
+
+//mutating
+fruit.reverse();
+
+
+console.log(fruit);
+
+
