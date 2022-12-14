d6b89839982d71da76e6c0fa86cfc3b92a440f0f
terryvilver <vilver.terry@gmail.com>
2022-12-14 18:30:12 +0100

    réalisation exercice basic

--- angular/basics-assignment-3-start/src/app/app.component.ts
+++ angular/basics-assignment-3-start/src/app/app.component.ts
@@ -3,7 +3,22 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styles: [`
+  .text-color {
+    color:white;
+  }
+`]
 })
 export class AppComponent {
+  flag:boolean = false;
+  dates:Object[] = [];
+  id:number=0;
+
+  onDisplay(){
+    console.log(this.flag);
+    this.id++;
+    this.flag = !this.flag;
+    this.dates.push({id:this.id, date:new Date()})
+  }
+
 }
