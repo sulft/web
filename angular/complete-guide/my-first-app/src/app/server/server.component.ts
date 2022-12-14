cb7a30008b7d6d2f26b984e2b736a7521d36665a
terryvilver <vilver.terry@gmail.com>
2022-12-14 17:41:17 +0100

    directives attribute, structural directives

---
+++ angular/complete-guide/my-first-app/src/app/server/server.component.ts
@@ -0,0 +1,29 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector:"app-server",
+    templateUrl:"server.component.html",
+    styles:[`
+        .online{
+            color:white;
+        }
+    `]
+})
+
+export class ServerComponent {
+    serverId:number = 10;
+    serverStatus:String = "offline";
+
+    constructor() {
+        this.serverStatus = Math.random() > 0.5 ? 'online' : 'offline';
+        console.log(this.serverStatus)
+    }
+
+    getServerStatus() {
+        return this.serverStatus
+    }
+    
+    getColor() {
+        return this.serverStatus === 'online' ? 'green' : 'red';
+    }
+}
