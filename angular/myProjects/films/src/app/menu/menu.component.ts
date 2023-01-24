46f08d2f39d0b2106fee385da1dacb4d48f6ee03
terryvilver <vilver.terry@gmail.com>
2023-01-24 13:57:00 +0100

    changing menu

--- angular/myProjects/films/src/app/menu/menu.component.ts
+++ angular/myProjects/films/src/app/menu/menu.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
-  styleUrls: ['./menu.component.css']
+  styleUrls: ['./menu.component.css'],
 })
-export class MenuComponent {
+export class MenuComponent implements OnInit{
+  logged:boolean;
 
+  constructor(private router: Router, private auth:AuthService) {}
+
+  ngOnInit(): void {
+    this.logged = this.auth.isLoggedin();
+  }
+
+  deconnexion() {
+    this.auth.logout();
+    this.router.navigate(['connexion']);
+  }
 }
