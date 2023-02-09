170df2e2a3408c1e21bd0b5b2a300b5a3db60dd7
terryvilver <vilver.terry@gmail.com>
2023-02-09 23:59:45 +0100

    Interface

---
+++ hackerRank/java/InterfaceJava.java
@@ -0,0 +1,31 @@
+// link exercice
+// https://www.hackerrank.com/challenges/java-interface/problem?isFullScreen=true&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen
+
+import java.util.*;
+import java.lang.*;
+
+public class InterfaceJava {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int number = sc.nextInt();
+
+        MyCalculator calculator = new MyCalculator();
+        System.out.println("I implemented: " + calculator.getClass().getInterfaces().getName());
+        System.out.println(calculator.divisor_sum(number));
+    } 
+}
+
+class MyCalculator implements AdvancedArithmetic {
+    public int divisor_sum(int n) {
+        int total = 0;
+        for(int i = 1;i <= n; i++) {
+            if (n%i==0) total += i;
+        }
+        return total;
+    }
+}
+
+interface AdvancedArithmetic {
+    int divisor_sum(int n);
+}
