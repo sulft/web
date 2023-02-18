90e91a5d5b56cd450427ca8e518795aa33c9c7e4
terryvilver <vilver.terry@gmail.com>
2023-02-18 23:11:23 +0100

    big integer beginning

---
+++ hackerRank/java/BigIntegerJava.java
@@ -0,0 +1,30 @@
+// link exercice
+// https://www.hackerrank.com/challenges/java-biginteger/problem?isFullScreen=true
+
+import java.util.*;
+import java.math.BigInteger;
+
+
+public class BigIntegerJava {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String a = sc.nextLine();
+        String b = sc.nextLine();
+
+        if(a.length()<200 && !a.contains("-")) {
+            if(b.length()<200 && !a.contains("-")) {
+                BigInteger num1 = new BigInteger(a,10);
+                BigInteger num2 = new BigInteger(b,10);
+
+                BigInteger total = num1 + num2;
+                System.out.println(total);
+
+                total = num1 * num2;
+                System.out.println(total);
+
+            }
+        }
+
+    }
+}
