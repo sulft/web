866cef68493216e66783d1ada964026318b72614
terryvilver <vilver.terry@gmail.com>
2023-03-01 01:46:55 +0100

    exercie prime checker terminé

--- hackerRank/java/PrimeChecker.java
+++ hackerRank/java/PrimeChecker.java
@@ -1,22 +1,57 @@
+// link exercice
+// https://www.hackerrank.com/challenges/prime-checker/problem?isFullScreen=true
+
 import java.util.*;
 
 public class PrimeChecker {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
+        int a[] = new int[1];
 
-        for(int i = 0; i < 2; i++) {
+        for(int i = 0; i < a.length; i++) {
             int input = sc.nextInt();
-            Boolean flag = true;
-            for(int j = 2; j < input; j++) {
-                if(j < input && input%j==0) {
+            a[i] = input;
+        }
+
+        for(int l = 0; l < a.length; l++) {
+            boolean isPrime = true;
+
+            for (int k = 2; k <= Math.sqrt(a[l]); k++) {
+                if (a[l] % k == 0) {
+                    isPrime = false;
                     break;
-                } else {
-                    System.out.println("cool" + j);
                 }
             }
+            
+            if (isPrime) {
+                for (int i = 2; i <= a[l]; i++) {
+                    if (!isPrime(i)) {
+                        continue;
+                    }
+                    System.out.printf(i + " ");
+                    if(i == a[l]){
+                        System.out.println();
+                    }
+                }
+
+            }
+
         }
 
         sc.close();
+    }
+
+    public static boolean isPrime(int num) {
+        if (num < 2) {
+            return false;
+        }
+      
+        for (int i = 2; i <= Math.sqrt(num); i++) {
+            if (num % i == 0) {
+                return false;
+            }
+        }
+        return true;
     }
 }
