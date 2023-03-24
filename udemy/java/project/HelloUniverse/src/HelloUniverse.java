31514512db031332a0506da3e133b5a43a7fd9c0
terryvilver <vilver.terry@gmail.com>
2023-03-24 13:59:42 +0100

    mini project variable, declaration, affectation et affichage

--- udemy/java/project/HelloUniverse/src/HelloUniverse.java
+++ udemy/java/project/HelloUniverse/src/HelloUniverse.java
@@ -1,5 +1,12 @@
 public class HelloUniverse {
   public static void main(String... args) {
-    System.out.println("Hello Universe!");
+    int nbPlanete;
+    nbPlanete = 9;
+    System.out.println("Aux dernières nouvelles, le nombre total de planètes dans le système solaire est de : ");
+    System.out.println(nbPlanete);
+
+    nbPlanete = 8;
+    System.out.println("Il y a quelques années cependant, elles étaient au nombre de : ");
+    System.out.print(nbPlanete);
   }
 }
