f0e5aa28d62f367134ae12a094dc4338c237a6a2
terryvilver <vilver.terry@gmail.com>
2023-03-24 15:39:02 +0100

    opérateur arithmétique

--- udemy/java/cours/HelloWorld/src/HelloWorld.java
+++ udemy/java/cours/HelloWorld/src/HelloWorld.java
@@ -1,20 +1,6 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        // utilisation d'un type objet
-        String maChaine;
-        maChaine = "Hello World!";
-        System.out.println(maChaine);
-
-        // Declaration de variable de type primitif
-        float prixEssence = 1.23f;
-        char initialPrenom;
-        boolean majeur;
-        int anneeDeNaissance;
-
-        // Affectation
-        initialPrenom = 'T';
-        anneeDeNaissance = 1998;
-        anneeDeNaissance = 1998;
-        majeur = true;
+      float resultat = 5f / (2 + 3);
+      System.out.print(resultat);
     }
 }
