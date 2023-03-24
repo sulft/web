c470dc6136489de660f1dc5aa417ddcefff7fc02
terryvilver <vilver.terry@gmail.com>
2023-03-24 13:53:30 +0100

    les types primitifs

--- udemy/java/cours/HelloWorld/src/HelloWorld.java
+++ udemy/java/cours/HelloWorld/src/HelloWorld.java
@@ -2,5 +2,19 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World!");
 
+        // déclaration
+        float prixEssence = 1.23f;
+        char initialPrenom;
+        boolean majeur;
+        int anneeDeNaissance;
+
+        // affectation
+        initialPrenom = 'T';
+        anneeDeNaissance = 1998;
+        anneeDeNaissance = 1998;
+        majeur = true;
+
+
+
     }
 }
