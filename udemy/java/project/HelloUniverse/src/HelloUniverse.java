ca1d51eded03f5501099afe8c127626aa24bb8eb
terryvilver <vilver.terry@gmail.com>
2023-03-24 14:59:50 +0100

    project 2 with String

--- udemy/java/project/HelloUniverse/src/HelloUniverse.java
+++ udemy/java/project/HelloUniverse/src/HelloUniverse.java
@@ -1,12 +1,16 @@
 public class HelloUniverse {
   public static void main(String... args) {
     int nbPlanete;
+    String maChaine;
+
+    maChaine = "Aux dernières nouvelles, le nombre total de planètes dans le système solaire est de : ";
     nbPlanete = 9;
-    System.out.println("Aux dernières nouvelles, le nombre total de planètes dans le système solaire est de : ");
+    System.out.println(maChaine);
     System.out.println(nbPlanete);
 
+    maChaine = "Il y a quelques années cependant, elles étaient au nombre de : ";
     nbPlanete = 8;
-    System.out.println("Il y a quelques années cependant, elles étaient au nombre de : ");
+    System.out.println(maChaine);
     System.out.print(nbPlanete);
   }
 }
