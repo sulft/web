6fc3599f19ae8dbd2576342f0997d67694850cf6
terryvilver <vilver.terry@gmail.com>
2023-03-24 15:49:17 +0100

    add exercice bureau de change dollar to euros

---
+++ udemy/java/project/BureauChange/src/BureauChange.java
@@ -0,0 +1,8 @@
+public class BureauChange {
+  public static void main(String[] args) {
+      short valeurEuros = 1500;
+      float tauxChange = 1.22f;
+      float renduDollar = valeurEuros * tauxChange;
+      System.out.println(renduDollar);
+  }
+}
