eedc40f4e2c6ca7efb0a29443bfbe7f41c2a2e30
terryvilver <vilver.terry@gmail.com>
2023-03-26 21:28:30 +0200

    application break and continue

--- udemy/java/cours/HelloWorld/src/HelloWorld.java
+++ udemy/java/cours/HelloWorld/src/HelloWorld.java
@@ -1,9 +1,18 @@
 public class HelloWorld {
     public static void main(String[] args) {
-      int nb = 0;
-      while (nb < 3) {
+      boolean jeContinue = true;
+      int nb = 1;
+      while (jeContinue) {
         System.out.println("Hello World!");
         nb++;
+        if (nb == 5) {
+          continue;
+        } // ce qui est après le if est ignoré
+        System.out.print("nb ne vaut pas 5");
+
+        if (nb == 10) {
+          break;
+        }
       }
 
     }
