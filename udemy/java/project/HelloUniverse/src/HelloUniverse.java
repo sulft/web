7fbc00e0084a2729569fe10e089261a3236351c9
terryvilver <vilver.terry@gmail.com>
2023-03-26 01:47:04 +0100

    application if else if else exercice

--- udemy/java/project/HelloUniverse/src/HelloUniverse.java
+++ udemy/java/project/HelloUniverse/src/HelloUniverse.java
@@ -1,11 +1,25 @@
 public class HelloUniverse {
   public static void main(String... args) {
+    String phraseIntroduction = "En %d, les planètes du système solaire étaient au nombre de : %d";
     int nbPlanetes;
     int annee;
 
-    annee = 2005;
+    annee = 1650;
 
     nbPlanetes = annee < 2014 ? 9 : 8;
-    System.out.printf("En %d, les planètes du système solaire étaient au nombre de : %d",annee, nbPlanetes);
+    if (annee >= 1600 && annee <= 2020) {
+      if (annee < 1700) {
+        nbPlanetes = 7;
+      } else if (annee < 1800) {
+        nbPlanetes = 8;
+      } else if (annee <= 2006) {
+        nbPlanetes = 9;
+      } else {
+        nbPlanetes = 8;
+      }
+      System.out.printf(phraseIntroduction, annee, nbPlanetes);
+    } else {
+      System.out.println("Notre programme ne peut pas fournir de résultat pour l'année " + annee);
+    }
   }
 }
