cf2aab13933005ef89ecbefcef3e914897745e16
terryvilver <vilver.terry@gmail.com>
2023-03-26 01:20:05 +0100

    if else if et else

--- udemy/java/cours/HelloWorld/src/HelloWorld.java
+++ udemy/java/cours/HelloWorld/src/HelloWorld.java
@@ -1,15 +1,17 @@
 public class HelloWorld {
     public static void main(String[] args) {
-      int age = 31;
-      System.out.printf("Voici mon age %d ",age);
+      int age = 24;
+      System.out.printf("Mon age est %d ans ",age);
 
       if(age >= 30){
         System.out.print("et je fais plus jeune.\n");
+      } else if(age < 10){
+        System.out.print("bravo à toi de commencer si jeune.\n");
+      } else if(age == 25){
+        System.out.print("et je suis en pleine forme.\n");
       } else {
-        System.out.print("et je suis jeune.\n");
+        System.out.println("et je suis fière de mon âge.");
       }
 
-      int ageDuCapitaine = 17;
-      System.out.print(ageDuCapitaine > 20 && age < 50 ? "L'age du capitaine est " + ageDuCapitaine : "Le code ne permet pas d'afficher l'age du capitaine pour une raison farfelue");
     }
 }
