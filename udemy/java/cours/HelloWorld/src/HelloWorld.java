d0cc06fa4a06a15561db360c87b36b217efd51fd
terryvilver <vilver.terry@gmail.com>
2023-03-26 21:12:03 +0200

    utilisation des while

--- udemy/java/cours/HelloWorld/src/HelloWorld.java
+++ udemy/java/cours/HelloWorld/src/HelloWorld.java
@@ -1,7 +1,9 @@
 public class HelloWorld {
     public static void main(String[] args) {
-      for (int i = 0; i < 3; i++) {
+      int nb = 0;
+      while (nb < 3) {
         System.out.println("Hello World!");
+        nb++;
       }
 
     }
