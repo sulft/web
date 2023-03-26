6af63bf1fbd59d1512d124c2f758734791b177ba
terryvilver <vilver.terry@gmail.com>
2023-03-26 03:08:47 +0200

    switch case syntax exercice

--- udemy/java/project/HelloUniverse/src/HelloUniverse.java
+++ udemy/java/project/HelloUniverse/src/HelloUniverse.java
@@ -1,25 +1,22 @@
 public class HelloUniverse {
   public static void main(String... args) {
-    String phraseIntroduction = "En %d, les planètes du système solaire étaient au nombre de : %d";
     int nbPlanetes;
-    int annee;
 
-    annee = 1650;
+    nbPlanetes = 99;
 
-    nbPlanetes = annee < 2014 ? 9 : 8;
-    if (annee >= 1600 && annee <= 2020) {
-      if (annee < 1700) {
-        nbPlanetes = 7;
-      } else if (annee < 1800) {
-        nbPlanetes = 8;
-      } else if (annee <= 2006) {
-        nbPlanetes = 9;
-      } else {
-        nbPlanetes = 8;
-      }
-      System.out.printf(phraseIntroduction, annee, nbPlanetes);
-    } else {
-      System.out.println("Notre programme ne peut pas fournir de résultat pour l'année " + annee);
+    switch (nbPlanetes) {
+      case 7:
+        System.out.print("On sait qu'au 16ème siècle, seules 7 planètes avaient été découvertes");
+        break;
+      case 8:
+        System.out.print("On sait que le nombre de planètes est passé de 7 à 8 au 17ème siècle, mais il a également été réduit de 9 à 8 en 2006");
+        break;
+      case 9:
+        System.out.print("On sait que le nombre de planètes est passé de 8 à 9 au 18ème siècle et ce jusqu'en 2006, où ce nombre a été réduit à 8");
+        break;
+      default:
+        System.out.printf("Le programme ne peut pas fournir de résultat pour " + nbPlanetes);
     }
+
   }
 }
