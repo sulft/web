2b45ee58189e3811306dc0a8f6cd98a799a9b591
terryvilver <vilver.terry@gmail.com>
2023-03-26 22:16:08 +0200

    exercice instanciation et changements de la valeur des propiétés de mon instance

--- udemy/java/project/HelloUniverse/src/HelloUniverse.java
+++ udemy/java/project/HelloUniverse/src/HelloUniverse.java
@@ -1,22 +1,11 @@
 public class HelloUniverse {
   public static void main(String... args) {
-    int nbPlanetes = 7;
+    Planet planet = new Planet();
 
-    while (nbPlanetes <= 9) {
-      switch (nbPlanetes) {
-        case 7:
-          System.out.println("On sait qu'au 16ème siècle, seules 7 planètes avaient été découvertes");
-          break;
-        case 8:
-          System.out.println("On sait que le nombre de planètes est passé de 7 à 8 au 17ème siècle, mais il a également été réduit de 9 à 8 en 2006");
-          break;
-        case 9:
-          System.out.println("On sait que le nombre de planètes est passé de 8 à 9 au 18ème siècle et ce jusqu'en 2006, où ce nombre a été réduit à 8");
-          break;
-        default:
-          System.out.printf("Le programme ne peut pas fournir de résultat pour " + nbPlanetes);
-      }
-      nbPlanetes++;
-    }
+    planet.kilometers = 142984;
+    planet.name = "Jupiter";
+    planet.material = "Gazeuse";
+
+    System.out.printf("%s est une planète %s avec un diamètre de %d kilomètres.",planet.name, planet.material,planet.kilometers);
   }
 }
