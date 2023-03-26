01505aee69eec72794c5d67c1dbab80ddd0e27d3
terryvilver <vilver.terry@gmail.com>
2023-03-26 21:07:27 +0200

    utilisation de la boucle for

--- udemy/java/cours/HelloWorld/src/HelloWorld.java
+++ udemy/java/cours/HelloWorld/src/HelloWorld.java
@@ -1,33 +1,7 @@
 public class HelloWorld {
     public static void main(String[] args) {
-      String mois;
-
-      mois = "Décembre";
-
-      switch(mois) {
-        case "Janvier":
-        case "Février":
-        case "Mars":
-          System.out.print("C'est l'hiver.");
-          break;
-        case "Avril":
-        case "Mai":
-        case "Juin":
-          System.out.print("C'est le printemps.");
-          break;
-        case "Juillet":
-        case "Aout":
-        case "Septembre":
-          System.out.print("C'est l'été.");
-          break;
-        case "Octobre":
-        case "Novembre":
-        case "Décembre":
-          System.out.print("C'est l'automne.");
-          break;
-        default:
-          System.out.println("Ce mois n'existe pas.");
-          break;
+      for (int i = 0; i < 3; i++) {
+        System.out.println("Hello World!");
       }
 
     }
