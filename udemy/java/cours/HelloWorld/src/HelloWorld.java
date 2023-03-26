ee6be7bd5105ad6ac2f035e2e0eb2a536351e92d
terryvilver <vilver.terry@gmail.com>
2023-03-26 01:07:49 +0100

    utilisation de condition ternaire

--- udemy/java/cours/HelloWorld/src/HelloWorld.java
+++ udemy/java/cours/HelloWorld/src/HelloWorld.java
@@ -4,9 +4,12 @@ public class HelloWorld {
       System.out.printf("Voici mon age %d ",age);
 
       if(age >= 30){
-        System.out.print("et je fais plus jeune.");
+        System.out.print("et je fais plus jeune.\n");
       } else {
-        System.out.print("et je suis jeune.");
+        System.out.print("et je suis jeune.\n");
       }
+
+      int ageDuCapitaine = 17;
+      System.out.print(ageDuCapitaine > 20 && age < 50 ? "L'age du capitaine est " + ageDuCapitaine : "Le code ne permet pas d'afficher l'age du capitaine pour une raison farfelue");
     }
 }
