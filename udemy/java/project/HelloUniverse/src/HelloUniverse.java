6db4b948180def4a42aa8812bd1ae6c54dd6de63
terryvilver <vilver.terry@gmail.com>
2023-03-28 12:42:58 +0200

    use of class method

--- udemy/java/project/HelloUniverse/src/HelloUniverse.java
+++ udemy/java/project/HelloUniverse/src/HelloUniverse.java
@@ -1,13 +1,42 @@
 public class HelloUniverse {
   public static void main(String... args) {
-    Planet planet = new Planet();
 
-    planet.kilometers = 142984;
-    planet.name = "Jupiter";
-    planet.material = "Gazeuse";
+    Planet mercure = new Planet();
+    mercure.name = "Mercure";
+    mercure.kilometers = 4880;
+    mercure.material = "Tellurique";
+    Planet venus = new Planet();
+    venus.name = "Venus";
+    venus.kilometers = 12100;
+    venus.material = "Tellurique";
+    Planet terre = new Planet();
+    terre.name = "Terre";
+    terre.kilometers = 12756;
+    terre.material = "Tellurique";
+    Planet mars = new Planet();
+    mars.name = "Mars";
+    mars.kilometers = 6792;
+    mars.material = "Tellurique";
+    Planet jupiter = new Planet();
+    jupiter.name = "Jupiter";
+    jupiter.kilometers = 142984;
+    jupiter.material = "Gazeuse";
+    Planet saturne = new Planet();
+    saturne.name = "Saturne";
+    saturne.kilometers = 120536;
+    saturne.material = "Gazeuse";
+    Planet uranus = new Planet();
+    uranus.name = "Uranus";
+    uranus.kilometers = 51118;
+    uranus.material = "Gazeuse";
+    Planet neptune = new Planet();
+    neptune.name = "Neptune";
+    neptune.kilometers = 49532;
+    neptune.material = "Gazeuse";
 
-    Planet newPlanet = new Planet();
+    neptune.spin();
+    mars.revolution();
 
-    System.out.printf("%s est une planète %s avec un diamètre de %d kilomètres.",newPlanet.name, newPlanet.material,newPlanet.kilometers);
+
   }
 }
--- udemy/java/project/HelloUniverse/src/Planet.java
+++ udemy/java/project/HelloUniverse/src/Planet.java
@@ -2,4 +2,12 @@ public class Planet {
   String name;
   String material;
   long kilometers;
+
+  void spin() {
+    System.out.println("Je suis la planète " + this.name + " et je tourne autour de mon étoile");
+  }
+
+  void revolution() {
+    System.out.println("je suis la planète " + this.name + " et je tourne sur moi-même. ");
+  }
 }
