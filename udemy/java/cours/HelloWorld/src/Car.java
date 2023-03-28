74d8329f882f9b0c88bc38923085e8098d29ca56
terryvilver <vilver.terry@gmail.com>
2023-03-28 13:01:13 +0200

    create method with and without parameter

--- udemy/java/cours/HelloWorld/src/Car.java
+++ udemy/java/cours/HelloWorld/src/Car.java
@@ -2,9 +2,24 @@ public class Car {
    byte nbDoor;
    boolean automatic;
    String color;
-
+   int rapportCourant;
    void Klaxonner() {
-     System.out.print("Tutut!!!");
+     System.out.println("Tutut!!!");
+   }
+   int Accélérer() {
+     System.out.println("J'accélère");
+     return 100;
+   }
+   int passerRapport(boolean augmenter) {
+      if(augmenter) {
+        rapportCourant++;
+      } else {
+        rapportCourant--;
+      }
+      return rapportCourant;
    }
 
+   void tourner(String droiteOuGauche, int angle) {
+     System.out.println("la tourner va tourner à " + droiteOuGauche + " avec un angle de " + angle + " degrés");
+   }
 }
--- udemy/java/cours/HelloWorld/src/HelloWorld.java
+++ udemy/java/cours/HelloWorld/src/HelloWorld.java
@@ -8,5 +8,16 @@ public class HelloWorld {
       System.out.println("La voiture est " + carOfTerry.color + " et dispose de " + carOfTerry.nbDoor + " portes.");
 
       carOfTerry.Klaxonner();
+
+      int nouveauRapport = carOfTerry.passerRapport(true);
+      nouveauRapport = carOfTerry.passerRapport(true);
+      nouveauRapport = carOfTerry.passerRapport(true);
+      nouveauRapport = carOfTerry.passerRapport(false);
+
+
+      System.out.println("Le nouveau rapport est : " + nouveauRapport);
+
+      carOfTerry.tourner("gauche",50);
     }
+
 }
