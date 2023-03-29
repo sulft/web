0623922e745b4bb720df4c941f49421974f24681
terryvilver <vilver.terry@gmail.com>
2023-03-30 00:29:50 +0200

    surcharge de méthode

--- udemy/java/cours/HelloWorld/src/Car.java
+++ udemy/java/cours/HelloWorld/src/Car.java
@@ -1,15 +1,22 @@
 public class Car {
    byte nbDoor;
+   int vitesseCourante = 30;
    boolean automatic;
    String color;
    int rapportCourant;
    void Klaxonner() {
      System.out.println("Tutut!!!");
    }
-   int Accélérer() {
-     System.out.println("J'accélère");
-     return 100;
+   int accélérer() {
+     int vitesse = 100;
+     System.out.println("J'accélère à " + vitesse + "km/h");
+     return vitesse;
    }
+
+    int accélérer(int vitesseEnPlus) {
+      System.out.println("J'accélère à " + (vitesseEnPlus + this.vitesseCourante) + "km/h");
+      return vitesseEnPlus + this.vitesseCourante;
+    }
    int passerRapport(boolean augmenter) {
       if(augmenter) {
         rapportCourant++;
@@ -22,4 +29,8 @@ public class Car {
    void tourner(String droiteOuGauche, int angle) {
      System.out.println("la tourner va tourner à " + droiteOuGauche + " avec un angle de " + angle + " degrés");
    }
+
+  void tourner(boolean droiteOuGauche, int angle) {
+    System.out.println("la tourner va tourner à " + droiteOuGauche + " avec un angle de " + angle + " degrés");
+  }
 }
--- udemy/java/cours/HelloWorld/src/HelloWorld.java
+++ udemy/java/cours/HelloWorld/src/HelloWorld.java
@@ -8,13 +8,9 @@ public class HelloWorld {
 
       System.out.println("La voiture est " + carOfTerry.color + " et dispose de " + carOfTerry.nbDoor + " portes.");
 
-      carOfTerry.Klaxonner();
+      carOfTerry.accélérer();
+      carOfTerry.accélérer(80);
 
-      String chaine1 = new String("test");
-      String chaine2 = new String("test");
-
-      boolean bool = chaine1 == chaine2;
-      System.out.println(bool);
     }
 
 }
