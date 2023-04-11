88567f9f144fa27fa7db7c40d71d11263dba8dfa
terryvilver <vilver.terry@gmail.com>
2023-04-11 02:38:04 +0200

    ngIf and ngFor lesson

--- angular/angular-up-and-running/cours/stock-market/src/app/stock/stock-item/stock-item.component.ts
+++ angular/angular-up-and-running/cours/stock-market/src/app/stock/stock-item/stock-item.component.ts
@@ -7,14 +7,24 @@ import { Stock } from 'src/app/model/stock';
   styleUrls: ['./stock-item.component.css'],
 })
 export class StockItemComponent {
-  stock: Stock;
+  stocks: Stock[];
 
   ngOnInit() {
-    this.stock = new Stock('Test Stock Company', 'TSC', 85, 80);
+    this.stocks= [
+      new Stock('Test Stock Company', 'TSC', 85, 80),
+      new Stock('Second Stock Company', 'SSC', 10, 20),
+      new Stock('Last Stock Company', 'LSC', 876, 765),
+    ]
+
+    // En utilisant ngStyle
+    // this.stockStyles = {
+    //   "color" : this.stock.isPositiveChange() ? "green" : "red",
+    //   "font-size" : largeChange ? "1.5em" : "0.8em"
+    // }
   }
 
-  toggleFavorite(event: any): void {
+  toggleFavorite(event: any, index:number): void {
     console.log(event);
-    this.stock.favorite = !this.stock.favorite;
+    this.stocks[index].favorite = !this.stocks[index].favorite
   }
 }
