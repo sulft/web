d78eab1e8930e1469212effee8469fb7ae601c5c
terryvilver <vilver.terry@gmail.com>
2023-04-19 22:07:52 +0200

    end exercice TDF

--- angular/angular-up-and-running/exercice/eCommmerce/src/app/add-product/add-product.component.ts
+++ angular/angular-up-and-running/exercice/eCommmerce/src/app/add-product/add-product.component.ts
@@ -1,24 +1,19 @@
 import { Component } from '@angular/core';
-import { NgForm } from '@angular/forms';
 import { Product } from '../model/product';
 
 @Component({
   selector: 'app-add-product',
   templateUrl: './add-product.component.html',
-  styleUrls: ['./add-product.component.css']
+  styleUrls: ['./add-product.component.css'],
 })
 export class AddProductComponent {
-  product:Product;
+  message:string = "";
 
-  ngOnInit() {
-    this.product = new Product("test",0,"",true)
-  }
 
-  submitForm(form:NgForm) {
-    console.log(this.product)
-
+  submitForm(form) {
     if(form.valid) {
-      this.product = form.value;
+       const product: Product = form.value[""];
+       console.log(product);
     }
   }
 }
