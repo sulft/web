c01402d83b586aafcc45ac87e9d62db901cefee7
terryvilver <vilver.terry@gmail.com>
2023-04-22 21:46:42 +0200

    using form group

--- angular/angular-up-and-running/cours/stock-market/src/app/stock/create-stock/create-stock.component.ts
+++ angular/angular-up-and-running/cours/stock-market/src/app/stock/create-stock/create-stock.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormControl } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-create-stock',
@@ -7,9 +7,13 @@ import { FormControl } from '@angular/forms';
   styleUrls: ['./create-stock.component.css']
 })
 export class CreateStockComponent {
-  public nameControl = new FormControl();
+  public stockForm = new FormGroup({
+    name: new FormControl(null, Validators.required),
+    code: new FormControl(null, [Validators.required, Validators.minLength(2)]),
+    price: new FormControl(null, [Validators.required, Validators.min(0)])
+  });
 
   onSubmit() {
-    console.log('name Control value', this.nameControl.value)
+    console.log('name Control value', this.stockForm.value)
   }
 }
