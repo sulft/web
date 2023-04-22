1b4ef57a3548b4281df33fc161853fa96cd4e460
terryvilver <vilver.terry@gmail.com>
2023-04-22 21:50:16 +0200

    getting rid of useless import

--- angular/angular-up-and-running/cours/stock-market/src/app/stock/create-stock/create-stock.component.ts
+++ angular/angular-up-and-running/cours/stock-market/src/app/stock/create-stock/create-stock.component.ts
@@ -1,19 +1,23 @@
 import { Component } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import {
+  FormBuilder,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-create-stock',
   templateUrl: './create-stock.component.html',
-  styleUrls: ['./create-stock.component.css']
+  styleUrls: ['./create-stock.component.css'],
 })
 export class CreateStockComponent {
-  public stockForm = new FormGroup({
-    name: new FormControl(null, Validators.required),
-    code: new FormControl(null, [Validators.required, Validators.minLength(2)]),
-    price: new FormControl(null, [Validators.required, Validators.min(0)])
+  public stockForm = this.fb.group({
+    name: [null, Validators.required],
+    code: [null, [Validators.required, Validators.minLength(2)]],
+    price: [null, [Validators.required, Validators.min(0)]],
   });
 
+  constructor(private fb: FormBuilder) {}
   onSubmit() {
-    console.log('name Control value', this.stockForm.value)
+    console.log('name Control value', this.stockForm.value);
   }
 }
